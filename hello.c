5faea97aed94a760055824d331062798eebcf6ea
Dung <Cuthihuong0681981@gmail.com>
2025-12-16 23:04:54 +0700

    Xóa file

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int  main(){
-    printf("Dung dep gai nhat sai gon!");
-    return 0;
-}
--- hello2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int  main(){
-    printf("Dung dep gai nhat quan 12!");
-    printf("Dung la number one!");
-
-    return 0;
-}
